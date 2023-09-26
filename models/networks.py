f97a71b9cffa3f5eddf86e5737eb7bd2f860d592
HanzhouLiu <marcohzliu@gmail.com>
2023-09-26 18:31:49 -0500

    module error

--- models/networks.py
+++ models/networks.py
@@ -5,11 +5,11 @@ from models import Stripformer, FSformer, FSformer_v0
 def get_generator(model_config):
     generator_name = model_config['g_name']
     if generator_name == 'Stripformer':
-        model_g = Stripformer()
+        model_g = Stripformer.Stripformer()
     elif generator_name == 'FSformer':
-        model_g = FSformer()
+        model_g = FSformer.FSformer()
     elif generator_name == 'FSformer_v0':
-        model_g = FSformer_v0()
+        model_g = FSformer_v0.FSformer_v0()
     else:
         raise ValueError("Generator Network [%s] not recognized." % generator_name)
     return nn.DataParallel(model_g)
