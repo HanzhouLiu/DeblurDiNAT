6999c3c01699be14a780e2c07fa43f32933afb35
HanzhouLiu <marcohzliu@gmail.com>
2023-10-09 19:00:35 -0500

    v5

--- models/networks.py
+++ models/networks.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from models import Stripformer, FSformer, FSformer_v0, FSformer_v1, FSformer_B, FSformer_B1, FSformer_B2, FSformer_V2, FSformer_V3, FSformer_V4
+from models import Stripformer, FSformer, FSformer_v0, FSformer_v1, FSformer_B, FSformer_B1, FSformer_B2, FSformer_V2, FSformer_V3, FSformer_V4, FSformer_V5
 
 def get_generator(model_config):
     generator_name = model_config['g_name']
@@ -24,6 +24,8 @@ def get_generator(model_config):
         model_g = FSformer_V3.FSformer_V3()
     elif generator_name == 'FSformer_V4':
         model_g = FSformer_V4.FSformer_V4()
+    elif generator_name == 'FSformer_V5':
+        model_g = FSformer_V5.FSformer_V5()
     else:
         raise ValueError("Generator Network [%s] not recognized." % generator_name)
     return nn.DataParallel(model_g)
