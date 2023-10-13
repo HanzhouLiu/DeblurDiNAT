62c08e2bd518f3a082bfdadfb0ae462dded71c42
HanzhouLiu <marcohzliu@gmail.com>
2023-10-12 23:03:27 -0500

    v2

--- models/networks.py
+++ models/networks.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from models import Stripformer, FSformer, FSformer_v0, FSformer_v1, FSformer_B, FSformer_B1, FSformer_B2, FSformer_V2, FSformer_V3, FSformer_V4, FSformer_V5, FSformer_V6, FSformer_V7, FSformer_V8, Freqformer
+from models import Stripformer, FSformer, FSformer_v0, FSformer_v1, FSformer_B, FSformer_B1, FSformer_B2, FSformer_V2, FSformer_V3, FSformer_V4, FSformer_V5, FSformer_V6, FSformer_V7, FSformer_V8, Freqformer, Freqformer_V0, Freqformer_V1, Freqformer_V2
 
 def get_generator(model_config):
     generator_name = model_config['g_name']
@@ -34,6 +34,12 @@ def get_generator(model_config):
         model_g = FSformer_V8.FSformer_V8()
     elif generator_name == 'Freqformer':
         model_g = Freqformer.Freqformer()
+    elif generator_name == 'Freqformer_V0':
+        model_g = Freqformer_V0.Freqformer_V0()
+    elif generator_name == 'Freqformer_V1':
+        model_g = Freqformer_V1.Freqformer_V1()
+    elif generator_name == 'Freqformer_V2':
+        model_g = Freqformer_V2.Freqformer_V2()
     else:
         raise ValueError("Generator Network [%s] not recognized." % generator_name)
     return nn.DataParallel(model_g)
