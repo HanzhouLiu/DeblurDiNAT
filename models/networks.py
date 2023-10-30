e1e48a4168f2368a001aa6b2a3fe4e90025b45a3
HanzhouLiu <marcohzliu@gmail.com>
2023-10-30 16:46:16 -0500

    channel controlled trans

--- models/networks.py
+++ models/networks.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from models import Stripformer, NADeblur_V0, NADeblur_V1, NADeblur_V2
+from models import Stripformer, NADeblur_V0, NADeblur_V1, NADeblur_V2, NADeblur_V3
 
 def get_generator(model_config):
     generator_name = model_config['g_name']
@@ -12,6 +12,8 @@ def get_generator(model_config):
         model_g = NADeblur_V1.NADeblur_V1()
     elif generator_name == 'NADeblur_V2':
         model_g = NADeblur_V2.NADeblur_V2()
+    elif generator_name == 'NADeblur_V3':
+        model_g = NADeblur_V3.NADeblur_V3()
     else:
         raise ValueError("Generator Network [%s] not recognized." % generator_name)
     return nn.DataParallel(model_g)
