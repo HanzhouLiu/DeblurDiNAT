ba635c3c65d2fbf0ab0091b17bceae276ad9d7bc
HanzhouLiu <marcohzliu@gmail.com>
2023-11-12 13:25:38 -0600

    v7

--- models/NADeblur_V7.py
+++ models/NADeblur_V7.py
@@ -110,35 +110,6 @@ class GDFN(nn.Module):
         x = self.project_out(x)
         return x
 
-class ChannelAttention(nn.Module):
-    def __init__(self, k_size=3):
-        super(ChannelAttention, self).__init__()
-        self.max_pool = nn.AdaptiveMaxPool2d(1)
-        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False) 
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        # feature descriptor on the global spatial information
-        y = self.max_pool(x)
-        # Two different branches of ECA module
-        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
-        # Multi-scale information fusion
-        y = self.sigmoid(y)
-
-        return x * y.expand_as(x)
-
-class SpatialAttention(nn.Module):
-    def __init__(self, kernel_size=7):
-        super(SpatialAttention, self).__init__()
-
-        self.conv = nn.Conv2d(1, 1, kernel_size, padding=kernel_size//2, bias=False)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        max_out, _ = torch.max(x, dim=1, keepdim=True)
-        max_out = self.conv(max_out)
-        return x * self.sigmoid(max_out)
-
     ##########################################################################
 ## High Freq Feed-Forward Network (HFFN)
 class HFFN(nn.Module):
