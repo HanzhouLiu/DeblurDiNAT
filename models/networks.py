1ede7683766fae78d960d4167ad8981b73a8664c
HanzhouLiu <marcohzliu@gmail.com>
2023-11-30 19:50:32 -0600

    dff

--- models/networks.py
+++ models/networks.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from models import Stripformer, NADeblur_V0, NADeblur_V1, NADeblur_V2, NADeblur_V3, NADeblur_V4, NADeblur_V5, NADeblur_V6, NADeblur_V7, NADeblur_V8, NADeblur_V9, NADeblur_V10
+from models import Stripformer, NADeblur_V0, NADeblur_V1, NADeblur_V2, NADeblur_V3, NADeblur_V4, NADeblur_V5, NADeblur_V6, NADeblur_V7, NADeblur_V8, NADeblur_V9, NADeblur_V10, NADeblur_V11
 
 def get_generator(model_config):
     generator_name = model_config['g_name']
@@ -28,6 +28,8 @@ def get_generator(model_config):
         model_g = NADeblur_V9.NADeblur_V9()
     elif generator_name == 'NADeblur_V10':
         model_g = NADeblur_V10.NADeblur_V10()
+    elif generator_name == 'NADeblur_V11':
+        model_g = NADeblur_V10.NADeblur_V11()
     else:
         raise ValueError("Generator Network [%s] not recognized." % generator_name)
     return nn.DataParallel(model_g)
