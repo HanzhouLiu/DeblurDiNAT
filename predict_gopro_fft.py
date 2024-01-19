0f8c659485768f8ba4b22fd8ae4bafad0a92b3ac
HanzhouLiu <marcohzliu@gmail.com>
2024-01-19 14:26:39 -0600

    fft test

--- predict_gopro_fft.py
+++ predict_gopro_fft.py
@@ -13,7 +13,7 @@ import torch.nn.functional as F
 
 def get_args():
     parser = argparse.ArgumentParser('Test an image')
-    parser.add_argument('--job_name', default='fsformer_without_fs',
+    parser.add_argument('--job_name', default='fsformer_with_skip_connections',
     type=str, help='current job s name')
     return parser.parse_args()
 
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     args = get_args()
     with open(os.path.join('config/', args.job_name, 'config_pretrained.yaml')) as cfg:
         config = yaml.safe_load(cfg)
-    blur_path = '/scratch/user/hanzhou1996/datasets/deblur/GOPRO_/test/blur'
+    blur_path = '/mnt/g/RESEARCH/PHD/Motion_Deblurred/datasets/GOPRO_/test/blur'
     out_path = os.path.join('results', args.job_name, 'images')
     weights_path = os.path.join('results', args.job_name, 'models', 'best_FSformer_pretrained.pth')
     if not os.path.isdir(out_path):
@@ -29,7 +29,6 @@ if __name__ == '__main__':
     model = get_generator(config['model'])
     model.load_state_dict(torch.load(weights_path))
     model = model.cuda()
-    #model.eval()
 
     test_time = 0
     iteration = 0
@@ -43,13 +42,16 @@ if __name__ == '__main__':
             warm_up += 1
             img = cv2.imread(blur_path + '/' + file + '/' + img_name)
             img_tensor = torch.from_numpy(np.transpose(img / 255, (2, 0, 1)).astype('float32')) - 0.5
-            b, c, h, w = img_tensor.shape
-            h_n = (32 - h % 32) % 32
-            w_n = (32 - w % 32) % 32
-            img_tensor = torch.nn.functional.pad(img_tensor, (0, w_n, 0, h_n), mode='reflect')
             with torch.no_grad():
                 img_tensor = Variable(img_tensor.unsqueeze(0)).cuda()
+                factor = 32
+                h, w = img_tensor.shape[2], img_tensor.shape[3]
+                H, W = ((h + factor) // factor) * factor, ((w + factor) // factor) * factor
+                padh = H - h if h % factor != 0 else 0
+                padw = W - w if w % factor != 0 else 0
+                img_tensor = F.pad(img_tensor, (0, padw, 0, padh), 'reflect')
                 result_image = model(img_tensor)
+                #result_image = result_image[:, :, :h, :w]
             if warm_up == 20:
                 break
         break
@@ -61,16 +63,20 @@ if __name__ == '__main__':
             img = cv2.imread(blur_path + '/' + file + '/' + img_name)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             img_tensor = torch.from_numpy(np.transpose(img / 255, (2, 0, 1)).astype('float32')) - 0.5
-            b, c, h, w = img_tensor.shape
-            h_n = (32 - h % 32) % 32
-            w_n = (32 - w % 32) % 32
-            img_tensor = torch.nn.functional.pad(img_tensor, (0, w_n, 0, h_n), mode='reflect')
             with torch.no_grad():
                 iteration += 1
                 img_tensor = Variable(img_tensor.unsqueeze(0)).cuda()
 
+                factor = 32
+                h, w = img_tensor.shape[2], img_tensor.shape[3]
+                H, W = ((h + factor) // factor) * factor, ((w + factor) // factor) * factor
+                padh = H - h if h % factor != 0 else 0
+                padw = W - w if w % factor != 0 else 0
+                img_tensor = F.pad(img_tensor, (0, padw, 0, padh), 'reflect')
+
                 start = time.time()
                 result_image = model(img_tensor)
+                result_image = result_image[:, :, :h, :w]
                 stop = time.time()
                 print('Image:{}/{}, CNN Runtime:{:.4f}'.format(iteration, total_image_number, (stop - start)))
                 test_time += stop - start
