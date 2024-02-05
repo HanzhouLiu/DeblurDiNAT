29f9ec066dfdf0a4dac080e7d7249f00942cf8a9
HanzhouLiu <marcohzliu@gmail.com>
2024-02-05 12:47:38 -0600

    uformer mprnet mimo

--- models/networks.py
+++ models/networks.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from models import FFTformer, Stripformer, NADeblur_V0, NADeblur_V1, NADeblur_V2, NADeblur_V3, NADeblur_V4, NADeblur_V5, NADeblur_V6, NADeblur_V7, NADeblur_V8, NADeblur_V9, NADeblur_V10, NADeblur_V11, NADeblur_V12, NADeblur_V13, NADeblur_V14, NADeblur_V15, NADeblur_V16, NADeblur_V17, NADeblur_V18, NADeblur_V19, NADeblur_V20, NADeblur_V21, NADeblur_V22, NADeblur_V23, NADeblur_V24, NADeblur, NADeblurL
+from models import MIMONet, MPRNet, Uformer, FFTformer, Stripformer, NADeblur_V0, NADeblur_V1, NADeblur_V2, NADeblur_V3, NADeblur_V4, NADeblur_V5, NADeblur_V6, NADeblur_V7, NADeblur_V8, NADeblur_V9, NADeblur_V10, NADeblur_V11, NADeblur_V12, NADeblur_V13, NADeblur_V14, NADeblur_V15, NADeblur_V16, NADeblur_V17, NADeblur_V18, NADeblur_V19, NADeblur_V20, NADeblur_V21, NADeblur_V22, NADeblur_V23, NADeblur_V24, NADeblur, NADeblurL
 def get_generator(model_config):
     generator_name = model_config['g_name']
     if generator_name == 'Stripformer':
@@ -61,6 +61,12 @@ def get_generator(model_config):
         model_g = NADeblurL.NADeblurL()
     elif generator_name == 'FFTformer':
         model_g = FFTformer.FFTformer()
+    elif generator_name == 'MIMONet':
+        model_g = MIMONet.MIMONet()
+    elif generator_name == 'FFTformer':
+        model_g = MPRNet.MPRNet()
+    elif generator_name == 'Uformer':
+        model_g = Uformer.Uformer()
     else:
         raise ValueError("Generator Network [%s] not recognized." % generator_name)
     return nn.DataParallel(model_g)
